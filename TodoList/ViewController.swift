fd11c75d358fe67d94768cb7899ce40b49eae536
Ribhu-7 <ribhughosh7@gmail.com>
2024-06-11 16:57:17 +0530

    thirdcommit

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -3,11 +3,12 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tableView: UITableView = UITableView()
-    var tasks: [String] = ["Breakfast","Job","Coding","Lunch","Workout","Sleep"]
+    var tasks: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //creating and adding tableview
         view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -16,30 +17,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         
         title = "Tasks"
+        
+        //title colour change
+        let textAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        navigationController?.navigationBar.titleTextAttributes = textAttributes
+        
+        //navbar colour change
+        self.navigationController?.navigationBar.backgroundColor = .systemIndigo
+        self.navigationController?.navigationBar.tintColor = .white
+        
+        //left side and right side navbar
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addClicked))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editClicked))
         
     }
     
+    //add button is clicked
     @objc func addClicked(){
         let alert = UIAlertController(title: "Add Task", message: nil, preferredStyle: .alert)
         
         alert.addTextField()
+        //ok clicked on alert
         let ok = UIAlertAction(title: "OK", style: .default) { (action) in
             
             let textField = alert.textFields![0]
-            self.tasks.append(textField.text!)
+            if textField.text?.isEmpty == false{
+                self.tasks.append(textField.text!)
+            }
+            
             self.tableView.reloadData()
         }
-        
+        //cancel click on alert
         let cancel = UIAlertAction(title: "cancel", style: .cancel) { (cancel) in
-            
+            //no need
         }
         alert.addAction(cancel)
         alert.addAction(ok)
         self.present(alert, animated: true, completion: nil)
     }
     
+    //edit clicked
     @objc func editClicked(){
         tableView.setEditing(!tableView.isEditing, animated: true)
         
@@ -51,21 +68,60 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    //swiping edit and delete properties
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        //delete item
         let delete = UIContextualAction(style: .destructive, title: "Delete"){ (action , view , success ) in
             self.tasks.remove(at: indexPath.row)
             self.tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
         }
-        let swipeActions = UISwipeActionsConfiguration(actions: [delete])
+        
+        //edit item
+        let edit = UIContextualAction(style: .normal, title:  "Edit",
+                                      handler: { [self] (ac:UIContextualAction, view:UIView, success:(Bool)
+                                                         -> Void) in
+            
+            // AlertView with Textfield for enter text
+            let alert = UIAlertController(title: "Want to Edit Item?",message: "",preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "NO", style: UIAlertAction.Style.default, handler: {
+                (alertAction: UIAlertAction!) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            
+            alert.addTextField { [self] (textField) in
+                textField.text = "\(tasks[indexPath.row])"
+            }
+            
+            alert.addAction(UIAlertAction(title: "YES", style: UIAlertAction.Style.default, handler: { [self]
+                (alertAction: UIAlertAction!) in
+                let textField = alert.textFields![0] // Force unwrapping because we know it exists.
+                if let i = tasks.firstIndex(of: "\(tasks[indexPath.row])") {
+                    tasks[i] = textField.text!
+                }
+                self.tableView.reloadData() // reload your tableview here
+            }))
+            
+            alert.view.tintColor = UIColor.black  // change text color of the buttons
+            alert.view.layer.cornerRadius = 25   // change corner radius
+            present(alert, animated: true, completion: nil)
+            
+            success(true)
+        })
+        
+        //            self.tasks.remove(at: indexPath.row)
+        //            self.tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        
+        let swipeActions = UISwipeActionsConfiguration(actions: [edit,delete])
         return swipeActions
     }
     
-    
+    //number of rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
     
-    // MARK: BASIC SETTING FOR TABLEVIEW
+    // data in each row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let task = tasks[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
